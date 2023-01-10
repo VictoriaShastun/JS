df42e3b870de3273ca8128fb9035f98aee62dd6e
VikaShastun <114591099+VictoriaShastun@users.noreply.github.com>
2023-01-11 00:15:13 +0200

    toFixed

---
+++ js/numbers.js
@@ -0,0 +1,34 @@
+// Метод шукає(парсить) перше потрапивше число(ціле обовязково) і виводить в консоль
+
+// let elementWidth = '50px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+// let elementWidth = '50px';
+
+// elementWidth = Number.parseInt(elementWidth);
+// console.log('elementWidth' , elementWidth)
+
+
+// Метод парсить дробне число ы виводить в консоль
+
+// let elementHeight = '280.75px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log('elementHeight' , elementHeight);
+
+
+// Метод шукає числа дробні і відсікає всі інші числа, а залишає тільки ті які ти поставиш в дужки toFixed
+
+// let salary = 1300.1562;
+// salary = salary.toFixed(2);
+// salary = Number(salary);
+// console.log(salary);
+
+// Можна написати коротше
+
+let salary = 1300.1562;
+salary = Number(salary.toFixed(2))
+console.log(salary);
+
+
+
